ad193d586b2bffdde09c41aadf6e7f8c690dd662
кирилл <shakhov-work@yandex.ru>
2023-12-21 17:35:09 +0300

    feat( VNavigation.vue ) created navigation component

---
+++ src/components/VNavigation/VNavigation.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="v-navigation">
+    <button class="v-button" @click="prevSlide">prev</button>
+    <button class="v-button" @click="nextSlide">next</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import { VSliderComposables } from "../Vslider/vSlider.composables.ts";
+
+const {
+  move,
+} = VSliderComposables();
+
+const prevSlide = () => move(-1);
+const nextSlide = () => move(1);
+</script>
+
+<style lang="sass">
+
+</style>
